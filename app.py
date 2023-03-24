fe02d8de94e7b6918d019eeb69e7d26c20729e9e
hannahker <hannah.ker96@gmail.com>
2023-03-23 17:11:45 -0700

    Update AG Grid with metadata from Redis

--- app.py
+++ app.py
@@ -7,14 +7,25 @@ from datetime import date
 import dash_mantine_components as dmc
 import dash_ag_grid as dag
 from dash_extensions.javascript import assign
-from utils.data_utils import get_image
+from utils.data_utils import get_image, update_df
+import warnings
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 app = dash.Dash(__name__)
 app.title = "Land cover analysis and classification"
 server = app.server  # expose server variable for Procfile
 
-df = px.data.stocks()
+df = update_df()
+
+columnDefs = [
+    {"field": "id", "checkboxSelection": True},
+    {"field": "date"},
+    {"field": "lat"},
+    {"field": "lon"},
+    {"field": "dim"},
+]
 
 # How to render geojson.
 point_to_layer = assign(
@@ -67,13 +78,21 @@ app.layout = ddk.App(
                         ddk.ControlItem(
                             label="Latitude",
                             children=dcc.Input(
-                                id="lat", min=-90, max=90, value=50.23
+                                id="lat",
+                                min=-90,
+                                max=90,
+                                value=50.23,
+                                type="number",
                             ),
                         ),
                         ddk.ControlItem(
                             label="Longitude",
                             children=dcc.Input(
-                                id="lon", min=-180, max=180, value=-120
+                                id="lon",
+                                min=-180,
+                                max=180,
+                                value=-120,
+                                type="number",
                             ),
                         ),
                         dmc.Space(h=50),
@@ -128,15 +147,12 @@ app.layout = ddk.App(
         ),
         ddk.Card(
             children=[
-                ddk.CardHeader(title="Select imagery to download"),
+                ddk.CardHeader(title="Select imagery to view"),
                 ddk.Row(
                     [
                         dag.AgGrid(
                             id="image-options",
-                            columnDefs=[
-                                {"headerName": i, "field": i}
-                                for i in df.columns
-                            ],
+                            columnDefs=columnDefs,
                             rowData=df.to_dict("records"),
                             columnSize="sizeToFit",
                             defaultColDef=dict(
@@ -146,7 +162,14 @@ app.layout = ddk.App(
                     ]
                 ),
                 dmc.Space(h=30),
-                dmc.Center(html.Button("Download selections", id="download")),
+                dmc.Center(
+                    [
+                        html.Button("Delete selection", id="delete"),
+                        html.Button("Display image", id="display"),
+                    ]
+                ),
+                html.Div(id="delete-div"),
+                html.Div(id="display-div"),
             ],
             style={"height": "550px"},
         ),
@@ -164,8 +187,35 @@ def loc_data(geojson):
     return geojson
 
 
+# TODO: Finish and merge with bigger callback
+@app.callback(
+    Output("delete-div", "children"),
+    Input("delete", "n_clicks"),
+    State("image-options", "selectedRows"),
+)
+def delete_img(n_clicks, selection):
+    if n_clicks:
+        print(selection)
+        return selection
+    return dash.no_update
+
+
+@app.callback(
+    Output("map-view", "center"),
+    Output("map-view", "zoom"),
+    Input("image-options", "selectedRows"),
+)
+def zoom_map(selection):
+    if selection:
+        print(selection[0]["lat"])
+        print(selection[0]["lon"])
+        return (float(selection[0]["lat"]), float(selection[0]["lon"])), 8
+    return dash.no_update
+
+
 @app.callback(
     Output("satellite-img", "children"),
+    Output("image-options", "rowData"),
     Input("get-data", "n_clicks"),
     State("my-date-picker", "value"),
     State("lat", "value"),
@@ -175,40 +225,41 @@ def loc_data(geojson):
 )
 def loc_data(n_clicks, date, lat, lon, dim):
     if n_clicks:
-        # lat, lon = geojson["features"][0]["geometry"]["coordinates"]
         image = get_image(lat, lon, dim, date)
         image_bounds = [
             [(lat - (dim / 2)), (lon - ((dim / 2)))],
             [(lat + (dim / 2)), (lon + ((dim / 2)))],
         ]
         if image:
-            print(image)
             img_overlay = (
                 dl.LayerGroup(
                     dl.ImageOverlay(
                         opacity=0.95,
-                        #url='assets/sample.png',
                         url=image,
                         bounds=image_bounds,
                     )
                 ),
             )
-            return img_overlay
+            df = update_df()
+            return img_overlay, df.to_dict("records")
         else:
-            return dcc.Markdown(
-                "Image is not available for the specified date or location."
+            return (
+                dcc.Markdown(
+                    "Image is not available for the specified date or location."
+                ),
+                dash.no_update,
             )
     else:
-        return dash.no_update
+        return dash.no_update, dash.no_update
 
 
-@app.callback(
-    Output("image-options", "rowData"), Input("get-data", "n_clicks")
-)
-def get_download_options(nclicks):
-    if nclicks:
-        print("get data!")
-    return dash.no_update
+# @app.callback(
+#     Output("image-options", "rowData"), Input("get-data", "n_clicks")
+# )
+# def get_download_options(nclicks):
+#     if nclicks:
+#         print("get data!")
+#     return dash.no_update
 
 
 if __name__ == "__main__":
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,6 +4,7 @@ import io
 import pickle
 from constants import redis_instance, REDIS_EXPIRE_SEC, NASA_KEY
 import json
+import pandas as pd
 
 
 def get_image(lat, lon, dim, date="2014-02-04"):
@@ -17,7 +18,7 @@ def get_image(lat, lon, dim, date="2014-02-04"):
         return None
     elif "id" in img_metadata.keys():
         img_id = img_metadata["id"]
-        if img_id in [x.decode('utf8') for x in redis_instance.keys()]:
+        if img_id in [x.decode("utf8") for x in redis_instance.keys()]:
             print("Image already stored in redis. Loading from cache.")
             img = pickle.loads(redis_instance.get(img_id))
             return img
@@ -26,7 +27,31 @@ def get_image(lat, lon, dim, date="2014-02-04"):
             img_data = requests.get(img_url).content
             image_bytes = io.BytesIO(img_data)
             img = PIL.Image.open(image_bytes)
-            redis_instance.set(id=img_id, value=pickle.dumps(img))
+            img_info = {
+                "lat": lat,
+                "lon": lon,
+                "dim": dim,
+                "date": date,
+                "id": img_id,
+            }
+
+            redis_instance.set(img_id, pickle.dumps(img))
+            redis_instance.set(f"{img_id}_metadata", pickle.dumps(img_info))
             redis_instance.expire(img_id, REDIS_EXPIRE_SEC)
+            redis_instance.expire(f"{img_id}_metadata", REDIS_EXPIRE_SEC)
+
             print("Image data successfully retrieved and stored in Redis.")
             return img
+
+
+def update_df(df=None):
+    df = (
+        pd.DataFrame(columns=["id", "date", "lat", "lon", "dim"])
+        if not df
+        else df
+    )
+    for key in redis_instance.keys("*_metadata"):
+        df = df.append(
+            pickle.loads(redis_instance.get(key)), ignore_index=True
+        )
+    return df
